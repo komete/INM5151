01bf69c19bba67b73d366e8d482f9facea57d411
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2016-03-07 14:19:38 -0500

    Ajout tests controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get Signin" do
+  test "should get connect" do
     get :connect
     assert_response :success
+    assert_select "title", "Sign in"
+
   end
 
 end
