a67a82083bcc3f515379f8a29ba1c0114b88926a
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2016-03-07 10:10:11 -0500

    Ajout: nouvelle fonctions controlleur pages + new Routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   def Acceuil
+
   end
   def Cartes
 
   end
   def Offres
+
+  end
+  def Recherches
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/Cartes'
+  get 'pages/Recherches'
+  get 'pages/Offres'
+  root 'pages#acceuil'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
