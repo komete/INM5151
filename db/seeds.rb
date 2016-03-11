ea7cbd05ac24f09921e40cce495ee91b4fe30e20
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2016-03-11 10:51:19 -0500

    MAJ tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Jeux de données #
+
+User.create!(nom: 'GUILLAUME', prenom: 'Rémi', poste: '1', email: 'remi.guillaume.montreal@gmail.com',
+                     codeEmploye: 'GUIR23078602', telephone: '438-876-0843', username: 'komete', administrateur: true,
+             password: 'admin1', password_confirmation: 'admin1', verified: true, verified_at: Time.zone.now)
+User.create!(nom: 'SAWADOGO', prenom: 'Fayçal', poste: '1', email: 'faycalsawadogo@gmail.com',
+                     codeEmploye: 'SAWA23098900', telephone: '438-935-8129', username: 'Gangsta', administrateur: true,
+              password: 'admin2', password_confirmation: 'admin2', verified: true, verified_at: Time.zone.now)
+User.create!(nom: 'BRIEN-LEJEUNE', prenom: 'Stéphanie', poste: '1', email: 'stephanielejeune41@gmail.com',
+              codeEmploye: 'BRIS01579306', telephone: '514-218-7720', username: 'Superwoman', administrateur: true,
+              password: 'admin3', password_confirmation: 'admin3', verified: true, verified_at: Time.zone.now)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(nom: 'SAWADOGO', prenom: 'Fayçal', poste: '1', email: 'faycalsawadogo@gmail.com',
+                 codeEmploye: 'SAWA23098900', telephone: '438-935-8129', username: 'Gangsta', administrateur: true,
+                 password: 'admin2', password_confirmation: 'admin2', verified: true, verified_at: Time.zone.now)
+  end
+
+  test "Doit être valide" do
+    assert @user.valid?
+  end
 end
