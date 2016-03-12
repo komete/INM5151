edd23cee373da981f35aa8782f3009c59aa4e826
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2016-03-12 09:40:18 -0500

    Maj tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,17 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(nom: 'SAWADOGO', prenom: 'Fayçal', poste: '1', email: 'faycalsawadogo@gmail.com',
-                 codeEmploye: 'SAWA23098900', telephone: '438-935-8129', username: 'Gangsta', administrateur: true,
-                 password: 'admin2', password_confirmation: 'admin2', verified: true, verified_at: Time.zone.now)
+    @user = User.new(nom: 'SOUDANI', prenom: 'Haikel', poste: '1', email: 'soudanio@gmail.com',
+                 codeEmploye: 'SOUH25128500', telephone: '514-666-6699', username: 'ooligan', administrateur: false,
+                 password: 'admin3', password_confirmation: 'admin3', verified: true, verified_at: Time.zone.now)
   end
 
   test "Doit être valide" do
     assert @user.valid?
+  end
+
+  test "Nom présent" do
+    @user = users(:remi)
+    assert_not_equal @user.nom, "Remi"
   end
 end
