c6a74e7ae4a360dab7a19f4de4ecd4089076c7a2
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2016-03-12 14:30:11 -0500

    Maj application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,34 @@
 //= require turbolinks
 //= require_tree .
 
-var myIndex = 0;
-carousel();
+( function( $ ) {
+    $( document ).ready(function() {
+        $('#cssmenu').prepend('<div id="menu-button">RoadQuest</div>');
+        $('#cssmenu #menu-button').on('click', function(){
+            var menu = $(this).next('ul');
+            if (menu.hasClass('open')) {
+                menu.removeClass('open');
+            }
+            else {
+                menu.addClass('open');
+            }
+        });
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+
+    });
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}
+        x[myIndex-1].style.display = "block";
+        setTimeout(carousel, 8000);
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 8000);
-}
+} )( jQuery );
+
