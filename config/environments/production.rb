179aab0e2fc5272ef11dbef54fae78312e3db096
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2016-03-14 18:00:19 -0400

    changement param production pour mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'young-depths-45769.herokuapp.com'
+  host = 'roadquest.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.sendgrid.net',
